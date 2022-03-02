4cc9a008f428f86ad039c4690658fd318776714c
JamsterMan <ben.brad.h@gmail.com>
2022-03-02 17:18:38 -0500

    fixed removing values
    -fixed not removing row 1
    -fixed infinite loop from checking wrong array

--- SudokuClone/Assets/Scripts/GameBoard.cs
+++ SudokuClone/Assets/Scripts/GameBoard.cs
@@ -68,9 +68,9 @@ public class GameBoard : MonoBehaviour
             {
                 if (gameBoard[row, col] == 0)
                 {
-                    int i = Random.Range(1, 9);//used to randomized the board
+                    int i = Random.Range(1, 10);//used to randomized the board
                     for (int value = i; value < i+size; value++) {
-                        if (CheckCorrectPlacement(row, col, (value % 9) + 1))
+                        if (CheckCorrectPlacement(row, col, (value % 9) + 1, gameBoard))
                         {
                             gameBoard[row, col] = (value%9)+1;
 
@@ -89,23 +89,23 @@ public class GameBoard : MonoBehaviour
         return false;
     }
     //checks if value is already in the row, col, or 3x3 square area
-    private bool CheckCorrectPlacement(int row, int col, int value)
+    private bool CheckCorrectPlacement(int row, int col, int value, int[,] board)
     {
         for(int j = 0; j < size; j++)//check the row for value
         {
-            if (gameBoard[row, j] == value && j != col)
+            if (board[row, j] == value && j != col)
                 return false;
         }
         for (int i = 0; i < size; i++)// check the column for value
         {
-            if (gameBoard[i, col] == value && i != row)
+            if (board[i, col] == value && i != row)
                 return false;
         }
         for(int i = 3*(row / 3); i < 3*(row / 3) + 3; i++)//rows of the 3x3 square that row, col exists in
         {
             for (int j = 3 * (col / 3); j < 3 * (col / 3) + 3; j++)//Cols of the 3x3 square that row, col exists in
             {
-                if (gameBoard[i, j] == value && (i != row || j != col))
+                if (board[i, j] == value && (i != row || j != col))
                     return false;
             }
         }
@@ -132,12 +132,12 @@ public class GameBoard : MonoBehaviour
         int attempts = removeAttempts;
         while(attempts > 0)
         {
-            int row = Random.Range(1, 9);
-            int col = Random.Range(1, 9);
+            int row = Random.Range(0, 9);
+            int col = Random.Range(0, 9);
             while(gameBoardCopy[row, col] == 0)
             {
-                row = Random.Range(1, 9);
-                col = Random.Range(1, 9);
+                row = Random.Range(0, 9);
+                col = Random.Range(0, 9);
             }
 
             int tempValue = gameBoardCopy[row, col];
@@ -164,7 +164,7 @@ public class GameBoard : MonoBehaviour
                 {
                     for (int value = 1; value < size+1; value++)//loop throgh possible values
                     {
-                        if (CheckCorrectPlacement(row, col, value))
+                        if (CheckCorrectPlacement(row, col, value, gameBoardCopy))
                         {
                             gameBoardCopy[row, col] = value;
 
